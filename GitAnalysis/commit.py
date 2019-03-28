4f903226c13e867096af3638f418ad5062f39657
Mohsen Laali <m3n.laali@gmail.com>
2019-03-28 12:31:50 +1100

    Git diff is fixed

--- GitAnalysis/commit.py
+++ GitAnalysis/commit.py
@@ -1,10 +1,9 @@
 import os
-import whatthepatch
 from GitAnalysis.lexical_analyzer import LexicalAnalyzer
 from unidiff import PatchSet
 from cStringIO import StringIO
 from pygments.util import ClassNotFound
-from unidiff import UnidiffParseError
+# from unidiff import UnidiffParseError
 
 
 class GitDiffEmptyException(Exception):
@@ -26,69 +25,42 @@ class CommitSpec:
 
     def diff_commit_pre_version(self, **kwargs):
         if self.commit.parents:
-            pre_commit = self.commit.repo.commit(self.pre_commit_hex_sha)
+            # Git ignore white space at the end of line, empty lines
+            diff_txt = self.commit.repo.git.diff(self.pre_commit_hex_sha, self.commit_hex_sha,
+                                                 ignore_blank_lines=True, ignore_space_at_eol=True, **kwargs)
         else:
             raise NoParentsCommitException(self.commit_hex_sha)
 
-        # Git ignore white space at the end of line, empty lines, renamed files, also copied files and also
-        diff_index = pre_commit.diff(self.commit_hex_sha, create_patch=True, ignore_blank_lines=True,
-                                     ignore_space_at_eol=True, diff_filter='crt', ignore_submodules=True, **kwargs)
-        # diff_index = self.commit.diff(self.pre_commit_hex_sha, create_patch=True, **kwargs)
-        return diff_index
+        return diff_txt
 
-    def pars_diff_wp(self):
-        diff_index = self.diff_commit_pre_version()
-        pars_diff_list = []
-        for idx in diff_index:
-            dif = idx.diff
-            for pars_diff in whatthepatch.parse_patch(str(dif)):
-                pars_diff_list.append(pars_diff)
-        return pars_diff_list
+    def pars_diff(self, diff_txt):
 
-    def pars_diff(self, diff_index):
-
-        if len(diff_index) > 1:
-            diff_txt = reduce(lambda x, y: str(x) + os.linesep + str(y), diff_index)
-        elif len(diff_index) == 1:
-            diff_txt = str(diff_index[0])
-        else:
+        # check the if diff is empty
+        if not diff_txt:
             raise GitDiffEmptyException(self.commit_hex_sha)
 
         patches = PatchSet(StringIO(diff_txt), encoding='utf-8')
 
         return patches
 
-    def changed_files(self):
-        """
-        Returned list of changed files
-
-        """
-        pars_diff_list = self.pars_diff_wp()
-        changed_files_list = []
-        for pars_diff in pars_diff_list:
-            changed_files_list.append(FileChanges(pars_diff, self.commit.hexsha))
-        return changed_files_list
-
     def content_analyzer(self, analyze_part='deleted'):
-        patch_set = self.pars_diff(diff_index=self.diff_commit_pre_version())
+        patch_sets = self.pars_diff(diff_txt=self.diff_commit_pre_version())
         tokens_list = []
         number_of_changes = 0
-        for patched_file in patch_set:
+        for patched_file in patch_sets:
             file_path = patched_file.path
 
             analyze_line_number_list = []
             analyze_hex_sha = ''
 
             if analyze_part == 'added':
-                analyze_line_number_list = map(lambda l_: l_.target_line_no,
-                                               [line for hunk in patched_file for line in hunk if line.is_added and
-                                                line.value.strip() != ''])
+                analyze_line_number_list = [line.target_line_no for hunk in patched_file
+                                            for line in hunk if line.is_added and line.value.strip() != '']
                 analyze_hex_sha = self.commit_hex_sha
 
             elif analyze_part == 'deleted':
-                analyze_line_number_list = map(lambda l_: l_.source_line_no,
-                                               [line for hunk in patched_file for line in hunk if line.is_removed and
-                                                line.value.strip() != ''])
+                analyze_line_number_list = [line.source_source_line_no for hunk in patched_file
+                                            for line in hunk if line.is_removed and line.value.strip() != '']
                 analyze_hex_sha = self.pre_commit_hex_sha
 
             if len(analyze_line_number_list) > 0:
@@ -107,7 +79,7 @@ class CommitSpec:
         blame_details_dictionary = {}
         total_added = 0
         total_deleted = 0
-        patch_set = self.pars_diff(diff_index=self.diff_commit_pre_version())
+        patch_set = self.pars_diff(diff_txt=self.diff_commit_pre_version())
 
         # Calculate total add and delete in the comment
         for patched_file in patch_set:
@@ -148,88 +120,6 @@ class CommitSpec:
                     blame_details_dictionary[blame_commit.hexsha] = blame_detail
 
         return blame_details_dictionary.values()
-
-
-class FileChanges:
-    def __init__(self, pars_diff, commit_sha):
-        self.commit_SHA = commit_sha
-        self.index_path = pars_diff.header.index_path
-        self.old_path = pars_diff.header.old_path.strip()
-        self.new_path = pars_diff.header.new_path.strip()
-        self.old_version = pars_diff.header.old_version
-        self.new_version = pars_diff.header.new_version
-        self.changes = pars_diff.changes
-
-    @property
-    def changed_file_name(self):
-        dev_null = '/dev/null'
-        deleted_lines = map(lambda l: l[2], self.deleted_lines)
-        added_lines = map(lambda l: l[2], self.added_lines)
-        print '_+_+_+_+'
-        for line in deleted_lines:
-            print line
-        print '=-=--=-=-'
-        if len(deleted_lines) == len(added_lines):
-            for ind in range(0, len(deleted_lines)):
-                if deleted_lines[ind] != added_lines[ind]:
-                    print '********************************'
-                    print deleted_lines[ind]
-                    print added_lines[ind]
-                    print '********************************'
-        for line in added_lines:
-            print line
-
-        if self.old_path != self.new_path and self.new_path != dev_null and self.old_path != dev_null:
-            if set(map(lambda l: l[2], self.deleted_lines)) == set(map(lambda l: l[2], self.added_lines)):
-                return True
-        return False
-
-    @property
-    def added_lines(self):
-        return filter(lambda line: line[1] is None and line[2].strip() != '', self.changes)
-
-    @property
-    def deleted_lines(self):
-        return filter(lambda line: line[0] is None and line[2].strip() != '', self.changes)
-
-    @property
-    def changed_lines(self):
-        return filter(lambda line: (line[0] is None or line[1] is None) and line[2].strip() != '', self.changes)
-
-    @property
-    def line_number_of_added_lines(self):
-        return [added_line[0] for added_line in self.added_lines]
-
-    @property
-    def line_number_of_deleted_lines(self):
-        return [deleted_line[1] for deleted_line in self.deleted_lines]
-
-    @property
-    def line_number_of_changed_lines(self):
-        return self.line_number_of_added_lines + self.line_number_of_deleted_lines
-
-    @property
-    def number_of_added_lines(self):
-        return len(self.added_lines)
-
-    @property
-    def number_of_deleted_lines(self):
-        return len(self.deleted_lines)
-
-    @property
-    def number_of_changed_lines(self):
-        return len(self.changed_lines)
-
-    def __str__(self):
-        content = 'index path: ' + str(self.index_path) + os.linesep
-        content += 'old path: ' + str(self.old_path) + os.linesep
-        content += 'new path: ' + str(self.new_path) + os.linesep
-        content += 'old version: ' + str(self.old_version) + os.linesep
-        content += 'new version: ' + str(self.new_version) + os.linesep
-        content += 'changes: ' + os.linesep
-        for change in self.changes:
-            content += str(change) + os.linesep
-        return content
 
 
 class BlameDetail:
